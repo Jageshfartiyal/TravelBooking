14e78e7504ae70c0eeb559cb49de819b5e579075
Jagesh fartiyal <jageshfartiyal9720@gmail.com>
2023-10-30 00:24:46 +0530

    hot-fix

--- src/routes/webhook.routes.js
+++ src/routes/webhook.routes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 
-router.post('/webhook', webhook.webhook);
+router.get('/webhook', webhook.webhook);
 router.get('/', webhook.checkStatus);
 
 module.exports = router;
